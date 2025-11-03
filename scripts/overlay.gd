664da76cfc3426998e74e8cb478834775efa214e
TheModidGuy <dominicstanek1@gmail.com>
2025-11-03 09:28:20 +0000

    backup 1

--- scripts/overlay.gd
+++ scripts/overlay.gd
@@ -1,5 +1,18 @@
 extends Sprite2D
 
+@onready var label_tile_type: Label = $Label_TileType
+@onready var label_movement_cost: Label = $Label_MovementCost
+@onready var label_position: Label = $Label_Position
+@onready var label_resource: Label = $Label_Resource
+
+#func update_tile_info(tile):
+	#if tile == null:
+		#clear_info()
+		#return
+	#label_tile_type.text = 
+
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
